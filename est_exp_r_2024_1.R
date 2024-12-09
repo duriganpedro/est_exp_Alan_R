6f69703d912941d45aee5b663d91cb1b3d4ec819
duriganpedro <pedrodurigan10@gmail.com>
2024-12-09 11:21:30 -0300

    Auto update

---
+++ est_exp_r_2024_1.R
@@ -0,0 +1,14 @@
+#Lista 01 - 09/12;2024
+#Pacotes carregados no ambiente
+(.packages())
+#listado todos os pacotes disponiveis para uso
+(.packages(all.available = TRUE))
+#Instalação de pacotes
+#aba Packages / Install / Procurar o Nome
+#Agricolae
+# Nortest
+# install.packages("nortest")
+#Lawstat
+# install.packages("lawstat")
+#tidyverse
+# install.packages("tidyverse")
